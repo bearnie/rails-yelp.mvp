5dbfc1100bdc99ef318ed64e49d0b32ad5d66291
bearnie <bearniezeborg@gmail.com>
2016-10-12 15:19:39 +0200

    Models tested and running

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true}, inclusion: { in: [0,1,2,3,4,5], allow_nil: false}
+  belongs_to :restaurant
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
+    stars:        1
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+    stars:        1
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
+    stars:        2
+  },
+  {
+    name:         "La belle poule",
+    address:      "Place Sébastopol 13004 Marseille",
+    description:  "Un spécialiste du poulet en plein Marseille...",
+    stars:        3
+  },
+  {
+    name:         "La Daurade charnue",
+    address:      "1 Quai des Belges 13001 Marseille",
+    description:  "Le poissonier par excellence...",
+    stars:        3
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
